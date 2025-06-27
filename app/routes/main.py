4f348dde9eb70d9b48181f23e69311da61149440
aleguy02 <alejandro.the.villate@gmail.com>
2025-06-27 11:35:30 -0400

    Cache static data
    This commit will avoid repeated file I/O

--- app/routes/main.py
+++ app/routes/main.py
@@ -7,20 +7,29 @@ main_bp: Blueprint = Blueprint("main", __name__)
 config = get_config()
 
 
+# Cache experience data
+experience_path = os.path.join(
+    os.path.dirname(__file__), "..", "data", "experience.json"
+)
+with open(experience_path, "r", encoding="utf-8") as f:
+    experience_data = json.load(f)
+
+# Cache hobbies data
+hobbies_path = os.path.join(os.path.dirname(__file__), "..", "data", "hobbies.json")
+with open(hobbies_path, "r", encoding="utf-8") as f:
+    hobbies_data = json.load(f)
+
+
 @main_bp.route("/")
 def index():
     """
     Returns home page
     """
-    path = os.path.join(os.path.dirname(__file__), "..", "data", "experience.json")
-    with open(path, "r", encoding="utf-8") as f:
-        work_experience = json.load(f)
-
     return render_template(
         "index.html",
         title="Alejandro Villate",
         url=config.URL,
-        work_experience=work_experience,
+        work_experience=experience_data,
         mapboxgl_pub=config.MAPBOX_API_KEY,
     )
 
@@ -30,8 +39,4 @@ def hobbies():
     """
     Returns hobbies page
     """
-    path = os.path.join(os.path.dirname(__file__), "..", "data", "hobbies.json")
-    with open(path, "r", encoding="utf-8") as f:
-        hobbies = json.load(f)
-
-    return render_template("hobbies.html", title="Hobbies", hobbies=hobbies)
+    return render_template("hobbies.html", title="Hobbies", hobbies=hobbies_data)
