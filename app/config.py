80d5b48bdf757bd47a17bd0b223515caa3d9e634
aleguy02 <alejandro.the.villate@gmail.com>
2025-06-30 12:59:58 -0400

    Move static data IO into config class

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,8 +10,18 @@ class Config:
     Base configuration class
     """
 
+    # Load environment variables
     URL = os.getenv("URL")
     MAPBOX_API_KEY = os.getenv("MAPBOX_API_KEY")
+
+    # Load static data
+    experience_path = os.path.join(os.path.dirname(__file__), "data", "experience.json")
+    with open(experience_path, "r", encoding="utf-8") as f:
+        experience_data = json.load(f)
+
+    hobbies_path = os.path.join(os.path.dirname(__file__), "data", "hobbies.json")
+    with open(hobbies_path, "r", encoding="utf-8") as f:
+        hobbies_data = json.load(f)
 
     @staticmethod
     def validate_env_vars():
@@ -18,6 +29,15 @@ class Config:
 
         if not Config.MAPBOX_API_KEY:
             missing_vars.append("MAPBOX_API_KEY")
+
+        if not Config.URL:
+            missing_vars.append("URL")
+
+        if not Config.experience_data:
+            missing_vars.append("experience_data")
+
+        if not Config.hobbies_data:
+            missing_vars.append("hobbies_data")
 
         if missing_vars:
             raise ValueError(
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,23 +1,8 @@
 from flask import Blueprint, render_template
 from app.config import get_config
-import json
-import os
 
 main_bp: Blueprint = Blueprint("main", __name__)
 config = get_config()
-
-
-# Cache experience data
-experience_path = os.path.join(
-    os.path.dirname(__file__), "..", "data", "experience.json"
-)
-with open(experience_path, "r", encoding="utf-8") as f:
-    experience_data = json.load(f)
-
-# Cache hobbies data
-hobbies_path = os.path.join(os.path.dirname(__file__), "..", "data", "hobbies.json")
-with open(hobbies_path, "r", encoding="utf-8") as f:
-    hobbies_data = json.load(f)
 
 
 @main_bp.route("/")
@@ -29,7 +14,7 @@ def index():
         "index.html",
         title="Alejandro Villate",
         url=config.URL,
-        work_experience=experience_data,
+        work_experience=config.experience_data,
         mapboxgl_pub=config.MAPBOX_API_KEY,
     )
 
@@ -39,4 +24,4 @@ def hobbies():
     """
     Returns hobbies page
     """
-    return render_template("hobbies.html", title="Hobbies", hobbies=hobbies_data)
+    return render_template("hobbies.html", title="Hobbies", hobbies=config.hobbies_data)
