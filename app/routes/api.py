71eb7d3545b84621db40e5b7d4686b32de92c35b
aleguy02 <alejandro.the.villate@gmail.com>
2025-07-13 18:18:44 -0400

    Add MySQL database

---
+++ app/routes/api.py
@@ -0,0 +1,57 @@
+"""
+API routes to interact with MySQL database
+"""
+
+import os, datetime
+from flask import Blueprint, request
+from playhouse.shortcuts import model_to_dict
+from app.models.timelinepost import TimelinePost
+
+api_bp: Blueprint = Blueprint("api", __name__)
+
+
+@api_bp.route("/api/timeline_post", methods=["POST"])
+def post_timeline_post():
+    """
+    Create element in TABLE timelinepost
+    """
+    name = request.form["name"]
+    email = request.form["email"]
+    content = request.form["content"]
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+
+@api_bp.route("/api/timeline_post", methods=["GET"])
+def get_timeline_post():
+    """
+    Fetch all elements in TABLE timelinepost
+    """
+    return {
+        "timeline_posts": [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+
+@api_bp.route("/api/timeline_post", methods=["DELETE"])
+def pop_timeline_post():
+    """
+    Pop most recently created element in TABLE timelinepost
+    """
+    latest_post = (
+        TimelinePost.select()
+        .order_by(
+            TimelinePost.created_at.desc(), TimelinePost.id.desc()
+        )  # if two posts were created at the same time, select the one with the higher id
+        .limit(1)
+        .first()
+    )
+
+    if latest_post:
+        latest_post.delete_instance()
+        return {"deleted": model_to_dict(latest_post)}, 200
+    else:
+        return {"error": "No posts to delete"}, 404
