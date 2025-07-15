a192e2951b304b939503826a1e5802935ce11997
aleguy02 <alejandro.the.villate@gmail.com>
2025-07-14 21:07:21 -0400

    Update redeploy-script.sh

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #
 # Written by : Alejandro Villate
-# Run this script from the root directory (i.e. one directory above PE-portfolio/)
+# Run this script from one directory above PE-portfolio/
 
-# Kill all existing tmux sessions
-tmux kill-server
+set -eu
 
 # Pull in latest changes
 cd PE-portfolio
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate && pip install -r requirements.txt
 
-# start a new detached tmux session to run app
-tmux new-session -ds "flask-app"
-tmux send-keys -t "flask-app" "source python3-virtualenv/bin/activate" C-m
-tmux send-keys -t "flask-app" "flask run --host=0.0.0.0" C-m
+# restart app service
+systemctl daemon-reload
+systemctl restart myportfolio
