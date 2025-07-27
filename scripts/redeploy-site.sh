32652efddd6edd8771271868882f94e13373d96a
aleguy02 <alejandro.the.villate@gmail.com>
2025-07-26 22:30:29 -0400

    Updated redeploy-site.sh to use Docker Compose

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -6,7 +6,6 @@ set -eo pipefail
 PROJECT_DIR="$HOME/PE-portfolio/"
 VENV_DIR="python3-virtualenv"
 URL="http://alejandrovillate.duckdns.org:5000/"
-SERVICE="myportfolio"
 MAX_RETRIES=5
 
 
@@ -17,16 +16,25 @@ git fetch && git reset origin/main --hard > /dev/null
 source $VENV_DIR/bin/activate && pip install -r requirements.txt > /dev/null
 
 
-echo "=== restarting service ==="
+echo "=== spinning down containers ==="
 
-systemctl daemon-reload
-systemctl restart $SERVICE
+docker compose -f compose.prod.yaml down > /dev/null
+
+
+echo "=== rebuilding containers ==="
+
+docker compose -f compose.prod.yaml up -d --build > /dev/null
 
 
 echo "=== validating service ==="
 
-if [ -z "$(systemctl status $SERVICE | grep "active (running)")" ]; then
-        echo "!! service not running !!"
+expected_containers='myportfolio
+running
+mysql
+running'
+
+if [ "$(docker ps --format 'json' | jq -r '.Names,.State')" != "$expected_containers" ]; then
+        echo "!! Unexpected container processes. !!"
         exit 1
 fi
 
@@ -37,17 +45,17 @@ while [ $retry_count -lt $MAX_RETRIES ]; do
                 echo "Health check passed"
                 break
         fi
-        
+
         retry_count=$((retry_count + 1))
         echo "Health check attempt $retry_count/$MAX_RETRIES failed"
-        
+
         if [ $retry_count -lt $MAX_RETRIES ]; then
                 echo "Retrying in 2 seconds..."
                 sleep 2
         fi
 done
 if [ $retry_count -eq $MAX_RETRIES ]; then
-        echo "Could not reach the site at $URL or received a non-200 HTTP response."
+        echo "!! Could not reach the site at $URL or received a non-200 HTTP response. !!"
         exit 1
 fi
 
