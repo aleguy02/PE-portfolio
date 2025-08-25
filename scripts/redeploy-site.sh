861aa04615f05d9b2280944143714c88b7ea2fc5
Alejandro Villate <145073504+aleguy02@users.noreply.github.com>
2025-08-24 22:13:58 -0400

    Remove pip install step from redeploy script
    Removed virtual environment activation and requirements installation from redeploy script.

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -13,7 +13,6 @@ echo "=== pulling in latest changes ==="
 
 cd $PROJECT_DIR
 git fetch && git reset origin/main --hard > /dev/null
-source $VENV_DIR/bin/activate && pip install -r requirements.txt > /dev/null
 
 
 echo "=== spinning down containers ==="
